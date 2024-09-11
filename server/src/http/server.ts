5b58b16d41e9867a3274baf1632ebf6ad0e6f41f
JRosa06 <contact@jrosa.me>
2024-09-11 12:35:08 +0100

    Server

---
+++ server/src/http/server.ts
@@ -0,0 +1,28 @@
+import fastify from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoalRoute } from "./routes/createGoal.routes";
+import { createCompletionRoute } from "./routes/createCompletion.routes";
+import { getPendingGoalsRoute } from "./routes/getPendingGoals.routes";
+import { getWeekSummaryRoute } from "./routes/getWeekSummary.routes";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
+
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP Server running!");
+	});
